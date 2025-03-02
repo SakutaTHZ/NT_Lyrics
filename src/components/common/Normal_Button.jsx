13ad7f681447215eb79c3eebfa1d7fec55173165
Thar Htet Zan <110976118+SakutaTHZ@users.noreply.github.com>
2025-03-02 11:22:09 +0630

    Base Frame setup for Login Page

--- src/components/common/Normal_Button.jsx
+++ src/components/common/Normal_Button.jsx
@@ -1,19 +1,28 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Normal_Button = ({custom_class, onClick, text='button'}) => {
-    return (
-        <>
-            <button className={`p-2 px-12 rounded-md cursor-pointer border ${custom_class}`} onClick={onClick}>
-                {text}
-            </button>
-        </>
-    )
-}
-
-Normal_Button.propTypes = {
-    custom_class: PropTypes.string,
-    onClick: PropTypes.func,
-    text: PropTypes.string,
+const Normal_Button = ({
+  custom_class,
+  onClick,
+  text = "button",
+  icon: Icon,
+}) => {
+  return (
+    <>
+      <button
+        className={`flex gap-2 items-center justify-center p-2 rounded-md cursor-pointer border ${custom_class}`}
+        onClick={onClick}
+      >
+        {Icon && <Icon />} {text}
+      </button>
+    </>
+  );
 };
 
-export default Normal_Button
+Normal_Button.propTypes = {
+  custom_class: PropTypes.string,
+  onClick: PropTypes.func,
+  text: PropTypes.string,
+  icon: PropTypes.elementType,
+};
+
+export default Normal_Button;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,49 @@
-const Login = () =>{
-    return (
-        <>
-            <p>Login Page</p>
-        </>
-    )
-}
+import Normal_Button from "../components/common/Normal_Button";
+import { FaGoogle } from "react-icons/fa";
+
+const Login = () => {
+  return (
+    <>
+      <div className="flex w-screen h-screen justify-center items-center">
+        <div className="loginContainer">
+          <h1>Welcome to <span>NT Lyrics & Chords</span></h1>
+          <p>Please Enter your Login details!</p>
+          <div className="flex flex-col">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              className="border"
+              placeholder="Enter your Email"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              id="password"
+              className="border"
+              placeholder="Enter your Password"
+            />
+          </div>
+          <div className="flex justify-between">
+            <div className="flex">
+              <input type="checkbox" id="remember" className="border" />
+              <label htmlFor="remember">Remember for 30 days</label>
+            </div>
+            <a href="*">Forgot password</a>
+          </div>
+          <Normal_Button custom_class={"w-full"} text="Get Started" />
+          <Normal_Button
+            custom_class={"w-full"}
+            icon={FaGoogle}
+            text="Sign In With Google"
+          />
+          <p>Don&apos;t have an Account? <a href="*">Sign up</a></p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Login;
