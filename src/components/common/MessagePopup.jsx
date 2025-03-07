a9ab622d5e15fba537a886fb2b8a257dd89f2a8d
Thar Htet Zan <110976118+SakutaTHZ@users.noreply.github.com>
2025-03-08 00:12:26 +0630

    added Message popup

---
+++ src/components/common/MessagePopup.jsx
@@ -0,0 +1,33 @@
+import { BiCheckCircle, BiErrorCircle } from "react-icons/bi";
+import PropTypes from "prop-types";
+import { FiAlertCircle } from "react-icons/fi";
+import { BsQuestionCircle } from "react-icons/bs";
+
+const MessagePopup = ({ custom_class = "", message_type = "", message_text = "There should be a message!" }) => {
+  return (
+    <>
+      <div
+        className={`animate-right-normal fixed top-16 right-0 shadow-sm flex items-center gap-4 md:gap-2 p-2 px-4 rounded-l-lg bg-white z-[90] w-fit max-w-3/4 ${custom_class}`}
+      >
+        {message_type === "error" ? (
+          <BiErrorCircle className="flex-shrink-0 text-red-500" size={20} />
+        ) : message_type === "alert" ? (
+          <FiAlertCircle className="flex-shrink-0 text-yellow-500" size={20} />
+        ) : message_type === "success" ? (
+          <BiCheckCircle className="flex-shrink-0 text-green-500" size={20} />
+        ) : (
+          <BsQuestionCircle className="flex-shrink-0 text-blue-400" size={20} />
+        )}
+        <p className="text-pretty">{message_text}</p>
+      </div>
+    </>
+  );
+};
+
+MessagePopup.propTypes = {
+  custom_class: PropTypes.string,
+  message_type: PropTypes.string,
+  message_text: PropTypes.string,
+};
+
+export default MessagePopup;
--- src/components/special/LyricsCard.jsx
+++ src/components/special/LyricsCard.jsx
@@ -1,19 +1,68 @@
 import sampleImage from "../../assets/images/Lyrics_sample.png";
-import { FaEye,FaRegHeart } from "react-icons/fa6";
+import { FaEye, FaRegHeart } from "react-icons/fa6";
 import Normal_Button from "../../components/common/Normal_Button"; // Adjust the path as necessary
+import { useState, useEffect } from "react";
+import { CgRemove } from "react-icons/cg";
+import MessagePopup from "../common/MessagePopup";
 
 const LyricsCard = () => {
+  const [showMessage, setShowMessage] = useState(false);
+  const [messageText, setMessageText] = useState("");
+  const [isInCollection, setIsInCollection] = useState(false);
+
+  const checkIfInCollection = () => {
+    // Check if the lyrics is in User's Collection
+    return setIsInCollection(false);
+  };
+
+  useEffect(() => {
+    checkIfInCollection();
+  }, []);
+
+  const changeLyricsStatus = () => {
+    if (isInCollection) {
+      setMessageText("Lyrics has been removed from the collection");
+    } else {
+      setMessageText("Lyrics has been added to the collection");
+    }
+  
+    setIsInCollection(!isInCollection);
+    setShowMessage(true);
+  
+    setTimeout(() => {
+      setShowMessage(false);
+    }, 2000);
+  };
+
   return (
     <>
+      {showMessage && (
+        <MessagePopup message_type={"success"} message_text={messageText} />
+      )}
       <div className="relative card w-full min-h-32 aspect-[4/5] rounded-md shadow-md border border-gray-200 overflow-hidden">
         <img src={sampleImage} className="w-full" />
         <div className="gradient-overlay w-full h-full absolute bottom-0"></div>
         <div className="flex justify-between w-full absolute bottom-0 p-2 px-4">
-            <div className="flex items-center gap-1 font-semibold">
-                <FaEye size={16} className="translate-y-[1px]"/> <span className="viewCount">1.2k</span>
-            </div>
+          <div className="flex items-center gap-1 font-semibold">
+            <FaEye size={16} className="translate-y-[1px]" />{" "}
+            <span className="viewCount">1.2k</span>
+          </div>
 
-            <Normal_Button icon={FaRegHeart} text="Add to collection" custom_class={`py-1 px-3 border-transparent shadow-sm bg-white`}/>
+          {isInCollection ? (
+            <Normal_Button
+              icon={CgRemove}
+              text="Remove from Collection"
+              custom_class={`py-1 px-3 border-transparent shadow-sm bg-red-50 text-red-500`}
+              onClick={changeLyricsStatus}
+            />
+          ) : (
+            <Normal_Button
+              icon={FaRegHeart}
+              text="Add to collection"
+              custom_class={`py-1 px-3 border-transparent shadow-sm bg-white`}
+              onClick={changeLyricsStatus}
+            />
+          )}
         </div>
       </div>
     </>
