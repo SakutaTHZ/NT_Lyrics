613b384ed7c00db4c653fa50f0f88de9cca70f18
Thar Htet Zan <110976118+SakutaTHZ@users.noreply.github.com>
2025-07-17 23:04:28 +0630

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
-        start_url: ".",
+        start_url: "/",
         icons: [
           {
             src: "pwa-192x192.png",
