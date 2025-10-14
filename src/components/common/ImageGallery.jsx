c1e5ea6ec48088a540acdc60021d3b4f6981280a
Thar Htet Zan <110976118+SakutaTHZ@users.noreply.github.com>
2025-10-14 14:57:56 +0630

    Update ImageGallery.jsx

--- src/components/common/ImageGallery.jsx
+++ src/components/common/ImageGallery.jsx
@@ -2,6 +2,7 @@ import { CgClose } from "react-icons/cg";
 import PropTypes from "prop-types";
 
 const ImageGallery = ({ lyric, setShowGallery, setImageError }) => {
+  console.log(lyric);
   return (
     <div className="animate-appear fixed inset-0 bg-[#00000090] backdrop-blur-md flex flex-col items-center justify-center z-[10000] p-2">
 
@@ -15,7 +16,7 @@ const ImageGallery = ({ lyric, setShowGallery, setImageError }) => {
 
       {/* Image Preview */}
       <img
-        src={lyric.lyricsPhoto}
+        src={lyric}
         alt="Lyrics"
         onError={() => setImageError(true)}
         onContextMenu={(e) => e.preventDefault()}
